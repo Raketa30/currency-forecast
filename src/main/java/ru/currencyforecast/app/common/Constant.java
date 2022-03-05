1922aa2dfffbebaa818ea2514fa30891f7cc4e5f
Igor Kulgachov <duckpool@yandex.ru>
2022-03-05 14:34:58 +0300

    feature: Добавлены зависимости opencsv, mockito, JUnit5, AssertJ
    добавлены константы валют и комманд в Constant
    
    обновлены файлы csv

--- src/main/java/ru/currencyforecast/app/common/Constant.java
+++ src/main/java/ru/currencyforecast/app/common/Constant.java
@@ -2,10 +2,13 @@ package ru.currencyforecast.app.common;
 
 public class Constant {
     //links
-    public static final String USD_CSV_LINK = "csv_data/USD_F01_02_2002_T01_02_2022.csv";
-    public static final String EUR_CSV_LINK = "csv_data/EUR_F01_02_2002_T01_02_2022.csv";
-    public static final String TRY_CSV_LINK = "csv_data/TRY_F01_02_2002_T01_02_2022.csv";
+    public static final String USD_CSV_LINK = "csv_data/USD_F01_02_2005_T05_03_2022.csv";
+    public static final String EUR_CSV_LINK = "csv_data/EUR_F01_02_2005_T05_03_2022.csv";
+    public static final String TRY_CSV_LINK = "csv_data/TRY_F01_02_2005_T05_03_2022.csv";
+    public static final String AMD_CSV_LINK = "csv_data/AMD_F01_02_2005_T05_03_2022.csv";
+    public static final String BGN_CSV_LINK = "csv_data/BGN_F01_02_2005_T05_03_2022.csv";
     //for csv
+    public static final String CSV_HEADER_NOMINAL = "nominal";
     public static final String CSV_HEADER_DATE = "data";
     public static final String CSV_HEADER_PRICE = "curs";
     public static final String CSV_HEADER_CURRENCY = "cdx";
@@ -17,10 +20,11 @@ public class Constant {
     public static final String CURRENCY_USD = "usd";
     public static final String CURRENCY_EUR = "eur";
     public static final String CURRENCY_TRY = "try";
+    public static final String CURRENCY_AMD = "amd";
+    public static final String CURRENCY_BGN = "bgn";
 
     public static final String FORECAST_DEPTH_WEEK = "week";
     public static final String FORECAST_DEPTH_TOMMOROW = "tomorrow";
-
 
     public static final int ALGORITHM_BASE_DAYS = 7;
     //messages
